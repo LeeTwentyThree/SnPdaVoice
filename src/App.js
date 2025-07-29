98e3ad2b6d967713c45ccf14ba25d7a2a95d7b67
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-07-29 11:08:37 -0400

    Add navigation/top bar and bio

--- src/App.js
+++ src/App.js
@@ -7,22 +7,65 @@ Modal.setAppElement('#root');
 function App() {
   return (
     <>
+      <TopBar />
       <div id="overlays" />
       <div id="page-content">
         <SiteHeader />
         <main>
           <GenerateMainSection />
         </main>
+        <hr/>
+        <SiteBio />
       </div>
       <SiteFooter />
     </>
   );
 }
 
+function TopBar() {
+  return (
+  <div className="topnav">
+    <h2 className="top-header">Subnautica PDA Voice Generator by Lee23</h2>
+    <div className="nav-links">
+      <a href="https://ko-fi.com/lee23" target="_blank" rel="noopener noreferrer">
+        <img
+          src="https://cdn.ko-fi.com/cdn/kofi5.png"
+          alt="Buy Me a Coffee at ko-fi.com"
+          height="36"
+          style={{ border: '0' }}
+        />
+      </a>
+    <a href="https://github.com/LeeTwentyThree/SnPdaVoice">Source code</a>
+    </div>
+  </div>
+  );
+}
+
+function SiteBio() {
+  return (
+    <div className="container">
+      <section className="bio">
+        <p>As someone who has been modding Subnautica for 6+ years, I have always wanted a free, accessible and quick way to
+          create the Subnautica PDA voice. AI-generated options generally do not sound accurate for replicating the filters. So, I created
+          this page to allow anyone to create their own custom PDA lines.
+        </p>
+        <p>I first started figuring out the PDA voice for <a href="https://www.nexusmods.com/subnautica/mods/1820">The Red Plague</a>,
+        which made establishing new, natural-feeling content so much easier. The technology I use for the Red Plague PDA might be slightly
+        more accurate, but I tend to use this site now because it's much faster.
+        </p>
+        <p>Getting the filters right has been difficult, taking me over 800 attempts, and unfortunately it still isn't perfect.
+          We don't know the original settings used, so an approximation is needed, and it can still be improved
+          over time (it's open source!).
+        </p>
+      </section>
+    </div>
+  )
+}
+
 function SiteHeader() {
   return (
     <div>
-      <h1>Subnautica PDA Voice Generator</h1>
+      <h1>PDA Voice Generator</h1>
       <hr />
     </div>
   );
@@ -136,6 +179,7 @@ function GenerateMainSection() {
           fontSize: '1rem',
         }}
       />
+      <div class='button-spacing'></div>
       <input
           type="checkbox"
           id="enable-ssml"
